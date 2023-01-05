e487bb4562e3a64bea1098724726ea86d28b7728
hcedenoindri <hcedenoindriago@gmail.com>
2023-01-04 21:31:01 -0600

    Updates

---
+++ precache-manifest.5ece43d0144c84f92b47543f3605f8c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8626e3354d53d185ce58cba777259c2f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4aec62509effa92052f2",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "51ed1d61376373737e40",
+    "url": "/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "4aec62509effa92052f2",
+    "url": "/static/js/2.0fdbe643.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.0fdbe643.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51ed1d61376373737e40",
+    "url": "/static/js/main.12f08c20.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-frontend-dev-portfolio/precache-manifest.ee50bc0a1fbb4aafab17a8b6801d6718.js"
+  "/precache-manifest.5ece43d0144c84f92b47543f3605f8c3.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-frontend-dev-portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
